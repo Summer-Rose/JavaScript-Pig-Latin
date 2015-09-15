9f0bda415536ed7f1be52e3560a09a3676ae3e97
Summer Brochtrup & Jennifer Morkunas <student@epicodus.com>
2015-09-15 13:48:09 -0700

    pass specs

--- spec/specs.js
+++ spec/specs.js
@@ -1,17 +1,9 @@
-// describe('toLowerCase', function() {
-//   it("converts entry to lower case", function() {
-//     expect(stripEntry("EpicoDus rOcKs!")).to.equal(result);
-//   });
-// });
-
-// describe('startsWithVowel', function() {
-//   it("Adds ay to end of word if it starts with a vowel", function() {
-//     expect(startsWithVowel("epicodus")).to.equal("epicodusay");
-//   });
-// });
-
-describe('startsWithCon', function() {
-  it("moves first letter of word to end of word and attaches 'ay'", function() {
-    expect(startsWithCon("psychology")).to.equal("ologypsychay");
+describe('pigLatinWord', function() {
+  it("moves consonants of word to end of word and attaches 'ay'", function() {
+    expect(pigLatinWord("psychology")).to.equal("ologypsychay");
   });
+
+  it("moves qu as a unit to the end of the word then attached 'ay'", function() {
+    expect(pigLatinWord("squeal")).to.equal("ealsquay");
+  })
 });
