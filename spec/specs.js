bcd8d57ed4f0dacd375d17af82d36f598ba772cc
Summer Brochtrup & Jennifer Morkunas <student@epicodus.com>
2015-09-15 10:27:58 -0700

    Add passing specs for functions in scripts.js

--- spec/specs.js
+++ spec/specs.js
@@ -1,5 +1,17 @@
-describe('functionName', function() {
-  it("describe behavior", function() {
-    expect(functionName(parameter)).to.equal(result);
+// describe('toLowerCase', function() {
+//   it("converts entry to lower case", function() {
+//     expect(stripEntry("EpicoDus rOcKs!")).to.equal(result);
+//   });
+// });
+
+describe('startsWithVowel', function() {
+  it("Adds ay to end of word if it starts with a vowel", function() {
+    expect(startsWithVowel("epicodus")).to.equal("epicodusay");
+  });
+});
+
+describe('startsWithCon', function() {
+  it("moves first letter of word to end of word and attaches 'ay'", function() {
+    expect(startsWithCon("piglatin")).to.equal("iglatinpay");
   });
 });
