0ba0bc9a48871d7c80c38441db032c92224cdfa7
Summer Brochtrup & Jennifer Morkunas <student@epicodus.com>
2015-09-15 12:22:51 -0700

    add startsWithCon function to move consonants to end of word

--- js/scripts.js
+++ js/scripts.js
@@ -1,24 +1,33 @@
 var vowels = ["a", "e", "i", "o", "u"];
 
-var pigLatin = function(){
+// var startsWithVowel = function(word) {
+//   for (var i = 0; i < vowels.length; i++) {
+//     if (word.charAt(0) == vowels[i]) {
+//       var newWord = word.concat("ay");
+//     }
+//   }
+//   return newWord;
+// };
 
-}
-
-var startsWithVowel = function(word) {
-  for (var i = 0; i < vowels.length; i++) {
-    if (word.charAt(0) == vowels[i]) {
-      var newWord = word.concat("ay");
-    }
-  }
-  return newWord;
-}
+// var startsWithCon = function(word) {
+//   for (var i = 0; i < vowels.length; i++) {
+//     if (word.charAt(0) !== vowels[i]) {
+//       var firstLetter = word.slice(0, 1);
+//       var newWord = word.substr(1).concat(firstLetter);
+//       debugger;
+//       return startsWithCon(newWord);
+//     } else {
+//       return word;
+//     }
+//   }
+// };
 
 var startsWithCon = function(word) {
-  for (var i = 0; i < vowels.length; i++) {
-    if (word.charAt(0) !== vowels[i]) {
-      var firstLetter = word.charAt(0);
-      var newWord = word.slice(1, word.length).concat(firstLetter).concat("ay");
+  if (vowels.indexOf(word.charAt(0)) == -1) {
+    var firstLetter = word.slice(0, 1);
+    var newWord = word.substr(1).concat(firstLetter);
+    return startsWithCon(newWord);
+  } else {
+      return word.concat("ay");
     }
-  }
-  return newWord;
-}
+};
