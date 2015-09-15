5e7e6709aa3feec4b72e24286c137cac9bf1ab3a
Summer Brochtrup & Jennifer Morkunas <student@epicodus.com>
2015-09-15 13:47:21 -0700

    account for 'qu' and refactored function

--- js/scripts.js
+++ js/scripts.js
@@ -1,32 +1,14 @@
 var vowels = ["a", "e", "i", "o", "u"];
 
-// var startsWithVowel = function(word) {
-//   for (var i = 0; i < vowels.length; i++) {
-//     if (word.charAt(0) == vowels[i]) {
-//       var newWord = word.concat("ay");
-//     }
-//   }
-//   return newWord;
-// };
-
-// var startsWithCon = function(word) {
-//   for (var i = 0; i < vowels.length; i++) {
-//     if (word.charAt(0) !== vowels[i]) {
-//       var firstLetter = word.slice(0, 1);
-//       var newWord = word.substr(1).concat(firstLetter);
-//       debugger;
-//       return startsWithCon(newWord);
-//     } else {
-//       return word;
-//     }
-//   }
-// };
-
-var startsWithCon = function(word) {
-  if (vowels.indexOf(word.charAt(0)) == -1) {
+var pigLatinWord = function(word) {
+  if (word.charAt(0) == "q" && word.charAt(1) == "u") {
+    var qu = word.slice(0, 2);
+    var newWord = word.substr(2).concat(qu);
+    return newWord.concat("ay");
+  } else if (vowels.indexOf(word.charAt(0)) == -1) {
     var firstLetter = word.slice(0, 1);
     var newWord = word.substr(1).concat(firstLetter);
-    return startsWithCon(newWord);
+    return pigLatinWord(newWord);
   } else {
       return word.concat("ay");
     }
